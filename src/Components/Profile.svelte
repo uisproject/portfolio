103238179ebc91d0b88f4198a4a558f0b0d21b7f
uisproject <uisprojectxyz@gmail.com>
2022-05-23 00:41:32 +0700

    added see more and fix some layout

--- src/Components/Profile.svelte
+++ src/Components/Profile.svelte
@@ -1,5 +1,63 @@
 <script>
+  import { gsap } from "gsap";
+  import gsapCore from "gsap/gsap-core";
+  import { onMount } from "svelte";
   const currentAge = new Date().getFullYear() - 2000;
+
+  // show more and less button
+  let showMore = true;
+  let isRunning = false;
+  const text =
+    "tech industry such as how to make a diagram before making an application, analyzing documents, requirements gathering and also develop some programs. Not to mention that I also like to do my own research about tech from Youtube, Udemy, Stackoverflow, etc.";
+
+  onMount(() => {
+    gsap.to(".block-type", { opacity: 0, duration: 1, delay: 1, repeat: -1 });
+  });
+
+  const showMoreHandle = () => {
+    const el = document.querySelector(".more-txt");
+
+    isRunning = true;
+
+    let letterIndex = 0;
+    let moreTextLength = el.innerHTML.length;
+
+    const runShowMore = () => {
+      setTimeout(() => {
+        if (letterIndex >= text.length) {
+          isRunning = false;
+          return;
+        }
+
+        el.innerHTML += text.charAt(letterIndex);
+        letterIndex += 1;
+
+        runShowMore();
+      }, 2);
+    };
+
+    const runShowLess = () => {
+      setTimeout(() => {
+        if (moreTextLength === 0) {
+          isRunning = false;
+          return;
+        }
+
+        moreTextLength--;
+        const updatedText = text.substring(0, moreTextLength);
+        el.innerHTML = updatedText;
+        runShowLess();
+      }, 2);
+    };
+
+    if (!showMore) {
+      showMore = true;
+      return runShowLess();
+    }
+
+    showMore = false;
+    return runShowMore();
+  };
 </script>
 
 <link rel="stylesheet" href="./styles/profile.css" />
@@ -19,16 +77,20 @@
             Studied at <strong>Bina Nusantara</strong> with
             <strong>Information System Degree</strong>
           </p>
-          <br />
-          <p>
+
+          <div class="education-story">
             At the year of 2018 I applied into Bina Nusantara University to
-            study more about tech industry such as how to make a diagram before
-            making an application, analyzing documents, requirements gathering
-            and also develop some programs. Not to mention that I also like to
-            do my own research about tech from Youtube, Udemy, Stackoverflow,
-            etc.
-          </p>
+            study more about
+            <span class="more-txt" />
+            <div class="block-type">|</div>
+          </div>
+          <button
+            style="margin-top: 20px;"
+            on:click={!isRunning ? showMoreHandle : null}
+            >Show {showMore ? "More" : "Less"}</button
+          >
         </div>
+        <br />
         <div class="skills-info">
           <h1 class="profile-title">Skills I have worked with</h1>
           <p>HTML & CSS, JavaScript, Java, PHP</p>
