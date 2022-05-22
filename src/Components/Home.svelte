e7e7b464c066740d86cf6b0ae12897b44904cc17
uisproject <uisprojectxyz@gmail.com>
2022-05-22 22:58:33 +0700

    added more contents at home

--- src/Components/Home.svelte
+++ src/Components/Home.svelte
@@ -11,9 +11,27 @@
       <div class="home-info">
         <h2>Hi, I am</h2>
         <h1 class="home-name">Louis Leonardo 🚀</h1>
+        <br />
         <p class="home-para">
-          A web developer that specialized in front-end development using
-          vanilla and various frameworks
+          A Web Developer that <span class="highlight"
+            >specialized in front-end development using vanilla and various
+            frameworks.</span
+          >
+          But i also want to tell you that
+          <span class="highlight"
+            >i have some experiences in backend field.</span
+          >
+          <br /> <br />
+          At the year of 2018 I applied into Bina Nusantara University to study more
+          about tech industry such as how to make a diagram before making an application,
+          analyzing documents, requirements gathering and also develop some programs.
+          Not to mention that I also like to do my own research about tech from Youtube,
+          Udemy, Stackoverflow, etc.
+
+          <br /><br />
+
+          My current focus is to create awesome product using my skill i've
+          learnt so far and becoming better and better😁.
         </p>
       </div>
     </div>
